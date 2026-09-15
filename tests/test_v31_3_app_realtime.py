eb91963e50eccf7569d395b9aaab15f05e25b6fc
Dukkud <skrulbu@gmail.com>
2026-09-15 19:16:17 +0500

    v32: update version regression expectation

--- tests/test_v31_3_app_realtime.py
+++ tests/test_v31_3_app_realtime.py
@@ -4,11 +4,12 @@ from src.realtime_training import ASSETS, AGENTS, TIMEFRAMES
 
 def test_health_is_paper_only():
     body = health()
-    assert body['version'] == '31.4.0'
+    assert body['version'] == '32.0.0'
     assert body['mode'] == 'paper'
     assert body['live_trading'] is False
     assert body['emergency_stop'] is True
     assert body['github_integrations'] == 'enabled_research_only'
+    assert body['edge_validation'] == 'research_only'
 
 
 def test_realtime_policy_exposes_all_assets_timeframes_and_agents():
